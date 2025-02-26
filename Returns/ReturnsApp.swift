52ac630c8b866124a562693070b28a64790b0f7e
jmstickney <jmstickney@gmail.com>
2025-02-25 20:20:14 -0500

    Initial Commit

---
+++ Returns/ReturnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReturnsApp.swift
+//  Returns
+//
+//  Created by Jonathan Stickney on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ReturnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
